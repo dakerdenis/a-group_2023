2b60ed7809552f6485c322fadc74a34807c4e5ea
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-15 09:43:17 +0400

    added

--- index.php
+++ index.php
@@ -17,16 +17,58 @@
                  <br>
                  раздел о нас на сайте,почему нас выбирают
              </div>
+             <!--------------->
              <!---Mobile__app__block---->
              <div class="main__mobile__block">
-                <p>
-                Раздел с мобильным приложением
-                </p>
-                <img src="./styles/imgs/1.png" alt="">
+                 <div class="mobile__block__wrapper">
+                     <!--? Block__with__text-->
+                     <div class="mobile__block__text__wrapper">
+                         <!--------------->
+                         <div class="mobile__text__name">
+                             <p>Friendly and helpful support</p>
+                             <p>when you need it</p>
+                         </div>
+                         <!--------------->
+                         <div class="mobile__text___text">
+                             <p>
+                                 Insurance is a tricky subject, but with Sanlam Indie, help is always at hand. Check out our Help Centre, get qualified assistance through live chat, or email us
+                             </p>
+                         </div>
+                         <!-------------->
+                         <div class="mobile__links__block">
+                             <a href="#">
+                                 <img src="./styles/imgs/appstore.png" alt="">
+                             </a>
+                             <a href="#">
+                                 <img src="./styles/imgs/googleplay.png" alt="">
+                             </a>
+                         </div>
+                     </div>
+                     <!--------------->
+                     <!--? Block__with_mobile----->
+                     <div class="mobile__images__block">
+                         <div class="mobile__images__wrapper">
+                             <!----sparcles--->
+                             <div class="mobile__sparcles__block">
+                                 <img src="./styles/imgs/228.svg" alt="">
+                             </div>
+                             <!---rectaangel--->
+                             <div class="mobile__rectaangel__block">
+                                 <img src="./styles/imgs/229.svg" alt="">
+                             </div>
+                             <!-----mobile------>
+                             <div class="mobile__mobile__block">
+                                 <img src="./styles/imgs/1.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                     <!--------------->
+                 </div>
              </div>
+             <!--------------->
              <!---our__clients__block---->
              <div class="main__clients__block">
-                    Нaши клиенты блок
+                 Нaши клиенты блок
              </div>
 
              <!---footer---->
