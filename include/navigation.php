deca8874b346ff8e69fd0527129b3588eead2874
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-16 15:55:44 +0400

    added

--- include/navigation.php
+++ include/navigation.php
@@ -1,5 +1,22 @@
 <div class="navigation__wrapper">
     <div class="navigation__container">
-        
+        <!---navigation logo---->
+        <div class="navigation__logo">
+            <img src="./styles/imgs/nav_logo.svg" alt="">
+        </div>
+        <!---nav call number 0909--->
+        <div class="navigation__call_number">
+            <p>*0909</p>
+        </div>
+
+        <!---online payment---->
+        <!----login link---->
+        <div class="navigation__login">
+
+        </div>
+        <!---language--->
+        <div class="navigation__language">
+
+        </div>
     </div>
 </div>
