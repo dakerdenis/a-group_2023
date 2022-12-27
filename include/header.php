105bff11e81849df03641eef4760fdbbbe4ccc74
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-27 11:24:20 +0400

    added

--- include/header.php
+++ include/header.php
@@ -5,12 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A-Group Sığorta Şirkəti</title>
-    <!---all styles---->
-    <link rel="stylesheet" href="./styles/style.css">
-    <!--header styles--->
-    <link rel="stylesheet" href="./styles/header.css">
-    <!----footer styles---->
-    <link rel="stylesheet" href="./styles/footer.css">
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!-- Link Swiper's CSS -->
@@ -18,4 +13,12 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
     />
+
+     <!--header styles--->
+    <link rel="stylesheet" href="./styles/header.css">
+    <!----footer styles---->
+    <link rel="stylesheet" href="./styles/footer.css">
+    <link rel="stylesheet" href="./styles/swiper.css">
+    <!---all styles---->
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
