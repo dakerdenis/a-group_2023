5ee3512f763172f866f7998b4261e8e4cb30794c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-08 19:27:33 +0400

    added

--- contact.php
+++ contact.php
@@ -8,8 +8,16 @@
     <?php
         include './include/navigation.php';
     ?>
-    <br><br><br><br><br><br><br><br><br><br>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam corrupti voluptatum deleniti voluptate omnis dolorem consectetur eos vitae veritatis esse voluptas reiciendis aperiam eius facilis ea, quod illo harum dignissimos?
+  <div class="contact__page__wrapper">
+    <!---главный блок на странице---->
+    <div class="contact__page__main__block">
+
+    </div>
+    <!---блок с инфой и картами--->
+    <div class="contact_page__map_info">
+        
+    </div>
+  </div>
 <?php
     include './include/footer.php';
 ?>
