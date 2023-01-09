831e3685dea87e04d1f6bb79d806548a0a036448
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-09 15:05:20 +0400

    added

--- contact.php
+++ contact.php
@@ -148,9 +148,7 @@ include './include/header.php';
                             <div id="Baku" class="tabcontent">
                                 
                             <iframe
-                             width="90%" 
-                             height="300" 
-                             frameborder="0" 
+
                              scrolling="no"
                               marginheight="0"
                               marginwidth="0" 
@@ -161,9 +159,7 @@ include './include/header.php';
                             <div id="Ganca" class="tabcontent">
                             <iframe 
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.944383130408!2d46.35611691540381!3d40.67519437933591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQwJzMwLjciTiA0NsKwMjEnMjkuOSJF!5e0!3m2!1sen!2s!4v1583846767311!5m2!1sen!2s" 
-                            width="90%" 
-                             height="300" 
-                             frameborder="0" 
+
                              scrolling="no"
                               marginheight="0"
                               marginwidth="0" 
@@ -176,7 +172,6 @@ include './include/header.php';
                         </div>
                     </div>
                 </div>
-
 
 
 
