29853449f0ded4ef382c1b2e8ae66f51975d210b
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-23 14:55:19 +0400

    added

--- faq.php
+++ faq.php
@@ -12,9 +12,46 @@ include './include/header.php';
     <?php
     include './include/navigation.php';
     ?>
-<div class="faq__page__wrapper">
-    
-</div>
+    <div class="faq__page__block">
+        <div class="faq__page__container">
+            <!----->
+            <div class="faq__page__name">
+                <p>FAQs</p>
+                <img src="" alt="">
+            </div>
+            <!------->
+            <div class="faq__page__wrapper">
+                <!---faq page element--->
+
+                <div class="faq__page__element">
+                    <div class="faq__page__element__wrapper">
+                        <!------->
+                        <div class="faq__page__element__name">
+                            <p>Həkimə müraciət proseduru necədir ?</p>
+                        </div>
+
+                        <!------->
+                        <div class="faq__page__element__desc">
+                            <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
+                                Müraciət üçün bir neçə üsul var: </p>
+                                
+                            <p>
+                                → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
+                            </p>
+
+                            <p>
+                                → Bizim internet saytımızda elektron müraciəti doldurmaq
+                            </p>
+
+                            <p>
+                                → Sığorta kartınızda olan nömrələrə zəng vurmaq
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
