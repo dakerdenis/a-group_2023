f456d0f276f538c0d9a677f67d4aa9272e539014
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-24 09:33:56 +0400

    added

--- about.php
+++ about.php
@@ -15,7 +15,7 @@ include './include/header.php';
     <div class="about__page__block">
         <div class="about__page__container">
 
-        <!---Şirkət haqqında----->
+            <!---Şirkət haqqında----->
             <div class="about__page__information">
                 <!------>
                 <div class="about__page__information__name">
@@ -26,23 +26,39 @@ include './include/header.php';
                 <div class="about__page__information__content">
                     <p>
                         A-QROUP SIĞORTA ŞİRKƏTİ 1995-ci ildə yaradılmışdır.
-                         A-Qroup özəl və 100% milli şirkətdir. 
-                         A-Qroup Sığorta Şirkəti Tibbi sığorta,
-                          Bədbəxt hadisələrdən fərdi sığorta, 
-                          Əmlak sığortası, Nəqliyyat vasitələrinin 
-                          sığortası, Xarici səfərlər zamanı sığorta, 
-                          Peşə məsuliyyəti sığortası və s. ilə sığorta 
-                          növləri ilə məşğul olur. Fəaliyyətinin başlıca 
-                          istiqamətini isə Tibbi sığorta təşkil edir. 
-                          Uzun illərdir ki A-Qroup Sığorta Şirkəti Azərbaycan sığorta bazarında Könüllü Tibbi Sığorta üzrə liderliyi əldə saxlayır. A-Qroup Sığorta Şirkətinin mükafatları: CIBC Award 2005 – dinamik maliyyə inkişafı, UGUR Award 2005 – İlin Sığorta Şirkəti. A-Qroup ISO: 9001/2008 standartlarına uyğ un keyfiyyətə nəzarət sistemi üzrə fəaliyyit göstərir. Sığorta proqramlarının inkişafında müştərilərimizin arzu və tövsiyələri, aparılan araşdırmaların və mütəxəssislərimizin uzun müddətli iş təcrübəsinin nəticələri nəzərə alınmışdır. Müştərilər üçün yüksək səviyyəli tibbi yardım təşkil etmək üçün şirkətimiz Bakı şəhərində və Azərbaycan Respublikasında fəaliyyət göstərən ən qabaqcıl klinikaların daxil olduğu xüsusi tibbi yardım sistemi işləyib hazırlamışdır. Bizimlə tibbi xidmət və xidmət standartları üzrə rəqabət apara biləcək digər bir müəssisənin olmadığından əminik. A-Q roup sığorta şirkəti müştərilərinin rahatlığını təmin etmək və etimadını qazanmaq üçün əlindən gələni əsir gəmir. Respublikamızda fəaliyyət göstərən BP, AZERCELL, EXXON, NOBEL OİL, TOTAL E&P, GÜRCÜSTAN, NIDERLAND, FRANSA, İSRAİL, İSVEÇ səfirlikləri və s. kimi iri şirkətlər öz əməkdaşları və onların ailə üzvlərinin sağlamlığını A-Qroup Sığorta Şirkətinə etibar etmişdir.
+                        A-Qroup özəl və 100% milli şirkətdir.
+                        A-Qroup Sığorta Şirkəti Tibbi sığorta,
+                        Bədbəxt hadisələrdən fərdi sığorta,
+                        Əmlak sığortası, Nəqliyyat vasitələrinin
+                        sığortası, Xarici səfərlər zamanı sığorta,
+                        Peşə məsuliyyəti sığortası və s. ilə sığorta
+                        növləri ilə məşğul olur. Fəaliyyətinin başlıca
+                        istiqamətini isə Tibbi sığorta təşkil edir.
+                        Uzun illərdir ki A-Qroup Sığorta Şirkəti Azərbaycan sığorta bazarında Könüllü Tibbi Sığorta üzrə liderliyi əldə saxlayır. A-Qroup Sığorta Şirkətinin mükafatları: CIBC Award 2005 – dinamik maliyyə inkişafı, UGUR Award 2005 – İlin Sığorta Şirkəti. A-Qroup ISO: 9001/2008 standartlarına uyğ un keyfiyyətə nəzarət sistemi üzrə fəaliyyit göstərir. Sığorta proqramlarının inkişafında müştərilərimizin arzu və tövsiyələri, aparılan araşdırmaların və mütəxəssislərimizin uzun müddətli iş təcrübəsinin nəticələri nəzərə alınmışdır. Müştərilər üçün yüksək səviyyəli tibbi yardım təşkil etmək üçün şirkətimiz Bakı şəhərində və Azərbaycan Respublikasında fəaliyyət göstərən ən qabaqcıl klinikaların daxil olduğu xüsusi tibbi yardım sistemi işləyib hazırlamışdır. Bizimlə tibbi xidmət və xidmət standartları üzrə rəqabət apara biləcək digər bir müəssisənin olmadığından əminik. A-Q roup sığorta şirkəti müştərilərinin rahatlığını təmin etmək və etimadını qazanmaq üçün əlindən gələni əsir gəmir. Respublikamızda fəaliyyət göstərən BP, AZERCELL, EXXON, NOBEL OİL, TOTAL E&P, GÜRCÜSTAN, NIDERLAND, FRANSA, İSRAİL, İSVEÇ səfirlikləri və s. kimi iri şirkətlər öz əməkdaşları və onların ailə üzvlərinin sağlamlığını A-Qroup Sığorta Şirkətinə etibar etmişdir.
                     </p>
                 </div>
 
             </div>
 
             <div class="about__page__chef">
+                <!----chefs----->
                 <div class="about__page__chef__name">
                     <p>Rəhbərlik</p>
+                    <img src="./styles/imgs/chef_blue.svg" alt="">
+                </div>
+                <div class="about__page__chef__container">
+                    <div class="about__page__chef__wrapper">
+                        <!----->
+                        <div class="__chef__element">
+                            <div class="__chef__element__container">
+                                <div class="__chef__element__photo">
+                                    <img src="./styles/imgs/chef__foto.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                    </div>
                 </div>
             </div>
 
