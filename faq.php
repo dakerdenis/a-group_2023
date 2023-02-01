a781d7f6c8947f81d0dca11e4001cb77e53cd638
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-01 13:13:10 +0400

    added

--- faq.php
+++ faq.php
@@ -16,7 +16,17 @@ include './include/header.php';
         <div class="faq__page__container">
             <!----->
             <div class="faq__page__name">
-                <p>FAQs</p>
+                <div class="page__name_wrapper">
+                    <div class="page_name_block1 page_name_block-faq">
+                        F
+                    </div>
+                    <div class="page_name_block2 page_name_block-faq">
+                        A
+                    </div>
+                    <div class="page_name_block3 page_name_block-faq">
+                        Qs
+                    </div>
+                </div>
                 <img src="./styles/imgs/faq_vector.svg" alt="">
             </div>
             <!------->
@@ -34,7 +44,7 @@ include './include/header.php';
                         <div class="faq__page__element__desc">
                             <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
                                 Müraciət üçün bir neçə üsul var: </p>
-                                
+
                             <p>
                                 → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
                             </p>
@@ -61,35 +71,7 @@ include './include/header.php';
                         <div class="faq__page__element__desc">
                             <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
                                 Müraciət üçün bir neçə üsul var: </p>
-                                
-                            <p>
-                                → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
-                            </p>
 
-                            <p>
-                                → Bizim internet saytımızda elektron müraciəti doldurmaq
-                            </p>
-
-                            <p>
-                                → Sığorta kartınızda olan nömrələrə zəng vurmaq
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="faq__page__element">
-                    <div class="faq__page__element__wrapper">
-                        <!------->
-                        <div class="faq__page__element__name">
-                            <p>Həkimə müraciət proseduru necədir ?</p>
-                        </div>
-
-                        <!------->
-                        <div class="faq__page__element__desc">
-                            <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
-                                Müraciət üçün bir neçə üsul var: </p>
-                                
                             <p>
                                 → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
                             </p>
@@ -117,7 +99,7 @@ include './include/header.php';
                         <div class="faq__page__element__desc">
                             <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
                                 Müraciət üçün bir neçə üsul var: </p>
-                                
+
                             <p>
                                 → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
                             </p>
@@ -145,7 +127,7 @@ include './include/header.php';
                         <div class="faq__page__element__desc">
                             <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
                                 Müraciət üçün bir neçə üsul var: </p>
-                                
+
                             <p>
                                 → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
                             </p>
@@ -173,7 +155,7 @@ include './include/header.php';
                         <div class="faq__page__element__desc">
                             <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
                                 Müraciət üçün bir neçə üsul var: </p>
-                                
+
                             <p>
                                 → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
                             </p>
@@ -189,7 +171,35 @@ include './include/header.php';
 
                     </div>
                 </div>
- 
+
+                <div class="faq__page__element">
+                    <div class="faq__page__element__wrapper">
+                        <!------->
+                        <div class="faq__page__element__name">
+                            <p>Həkimə müraciət proseduru necədir ?</p>
+                        </div>
+
+                        <!------->
+                        <div class="faq__page__element__desc">
+                            <p> Həkimə müraciət etmək üçün, sığorta şirkəti ilə əlaqə saxlayıb, səhhətinizdə olan şikayətlər əsasında Sizi həkim qəbuluna yazacağıq.
+                                Müraciət üçün bir neçə üsul var: </p>
+
+                            <p>
+                                → Bizim mobil tətbiqetməmizi telefona yükləmək və elektron müraciəti doldurmaq (şikayətlər, mütəxəssis seçimi, klinika və müayinə vaxtının seçimi)
+                            </p>
+
+                            <p>
+                                → Bizim internet saytımızda elektron müraciəti doldurmaq
+                            </p>
+
+                            <p>
+                                → Sığorta kartınızda olan nömrələrə zəng vurmaq
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
