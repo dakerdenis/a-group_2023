fdc138fca76286e4f81d18eb629f4d43eae498fa
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-07 11:05:11 +0400

    added

--- contact.php
+++ contact.php
@@ -235,6 +235,29 @@ include './include/header.php';
                         </p>
                     </div>
 
+
+                    <div class="popup__content__input">
+                        <div class="custom-select" >
+                            <select name="cars" id="cars" required>
+                                <option value="0">Select car</option>
+                                <option value="1">Audi</option>
+                                <option value="2">BMW</option>
+                                <option value="3">Citroen</option>
+                                <option value="4">Ford</option>
+                                <option value="5">Honda</option>
+                                <option value="6">Jaguar</option>
+                                <option value="7">Land Rover</option>
+                                <option value="8">Mercedes</option>
+                                <option value="9">Mini</option>
+                                <option value="10">Nissan</option>
+                                <option value="11">Toyota</option>
+                                <option value="12">Volvo</option>
+                            </select>
+                        </div>
+                    </div>
+
+
+
                     <div class="popup__content__input">
                         <input name="name" type="text" placeholder="Ad">
                     </div>
@@ -309,7 +332,7 @@ include './include/header.php';
                     "Bonjour",
                     "Shalom",
                     "Guten Tag",
-                    "Hallo",    
+                    "Hallo",
                 ],
                 i = 0;
             setInterval(function() {
