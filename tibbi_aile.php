ee0db44e511db0cfc8a20831b0ac753acfba5112
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-17 10:45:02 +0400

    added

--- tibbi_aile.php
+++ tibbi_aile.php
@@ -6,7 +6,7 @@ include './include/header.php';
 
 <style>
     <?php
-    include './styles/tibbi.css';
+    include './styles/tibbi_calc.css';
     ?>
 </style>
 
@@ -20,30 +20,50 @@ include './include/header.php';
         <div class="polis__count__wrapper">
             <div class="polis__count__name">
                 <div class="page__name_wrapper">
-                    <div class="page_name_block1 page_name_block_tibbi">
-                        <p>Tib</p>
+                    <div class="page_name_block1 page_name_block_tibbi_calc ">
+                        <p>FƏRDİ </p>
                     </div>
-                    <div class="page_name_block2 page_name_block_tibbi">
-                        <p>bi &nbsp;</p>
+                    <div class="page_name_block2 page_name_block_tibbi_calc">
+                        <p>Tibbi </p>
                     </div>
-                    <div class="page_name_block3 ">
-                        <p>Sığor</p>
+                    <div class="page_name_block3 page_name_block_tibbi_calc">
+                        <p>sığortanı </p>
                     </div>
-                    <div class="page_name_block4 page_name_block_tibbi">
-                        <p>ta</p>
+                    <div class="page_name_block4 page_name_block_tibbi_calc">
+                        <p>Hesabla</p>
                     </div>
                 </div>
                 <img src="./styles/imgs/tibbi_blue.svg" alt="">
             </div>
 
 
-             <div class="polis__coun__desc">
+             <div class="polis__count__desc">
                 <p>
                     <img src="./styles/imgs/alert-circle.svg" alt="">
                 Diqqət!
                 </p>
                 <p> Özəl proqram yalnız 1 şəxs üçün nəzərdə tutulub</p>
              </div>   
+
+
+             <div class="polis__count-form">
+                <form action="">
+                    <!---------Ad-------------->
+                    <div class="polis__count-form__input_element">
+                        <!--input desc-->
+                        <div class="polis_input_element_desc">
+                            <p>Ad</p>
+                        </div>
+                        <!---input data--->
+                        <div class="polis_input_element_input">
+                            <input type="text">
+                        </div>
+                    </div>
+
+
+
+                </form>
+             </div>
         </div>
     </div>
 
