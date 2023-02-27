5520ce7285c3f4c17942badfa1917d1f1902a881
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-27 14:37:39 +0400

    added

--- ferdi_auto.php
+++ ferdi_auto.php
@@ -6,20 +6,29 @@ include './include/header.php';
 
 <style>
     <?php
-    include './styles/tibbi_calc.css';
+    include './styles/ferdi_auto.css';
     ?>
 </style>
 
 <div class="main__wrapper">
 
-<?php
+    <?php
     include './include/navigation.php';
     ?>
-    asfafsf
-    <br><br><br><br><br><br><br><br><br><br>
-    asfdasf
+    <div class="ferdi__auto">
+        <br><br><br><br><br><br><br><br><br><br>
+        <div class="ferdi_auto_name">
+            <div class="page__name_wrapper">
+                <div class="page_name_block1">
+                Avtomobil  
+                </div>
+                <div class="page_name_block2">
+                 Sığortası
+                </div>
+            </div>
+        </div>
+    </div>
 
-<?php
+    <?php
     include './include/footer.php';
     ?>
-
