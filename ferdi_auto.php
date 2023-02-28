6d6b1358828e1e719ca88df3bf5d18bb32ddde54
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-28 09:33:06 +0400

    added

--- ferdi_auto.php
+++ ferdi_auto.php
@@ -39,18 +39,22 @@ include './include/header.php';
             </div>
             <!---ferdi avto text---->
             <div class="ferdi__auto__desc_text">
-                <p> KASKO - avtomobil sığortasının ən populyar və actual növlərindən biridir.</p>
-                <p> A-Qroup aşağıdakı hallarda sığortalanmış avtomobilin və ya onun ayrı-ayrı detallarının,
-                    hissələrinin zədələnməsi, tam məhvi v ə ya itkisi nəticəsində yaranmış ziyanı ödəməyə təminat verir:</p>
+                <div class="ferdi__auto__desc_container">
+                    <p> KASKO - avtomobil sığortasının ən populyar və actual növlərindən biridir.</p>
+                    <p> A-Qroup aşağıdakı hallarda sığortalanmış avtomobilin və ya onun ayrı-ayrı detallarının,
+                        hissələrinin zədələnməsi, tam məhvi v ə ya itkisi nəticəsində yaranmış ziyanı ödəməyə təminat verir:</p>
+                </div>
             </div>
 
             <div class="ferdi__auto__desc_text">
-                <p>Qəza (toqquşma, qarşılaşma zədəsi, çevrilmə, yuvarlanma)</p>
-                <p>Yanğın, öz-özünə alışma</p>
-                <p>Şüşələrin sınması</p>
-                <p>Təbii fəlakətlər</p>
-                <p>Nəqliyyat vasitəsinin qaçırılması, qaçırılma vaxtı baş verən zədələr və üçüncü şəxslərin digər qeyri qanuni hərəkətləri</p>
-                <p>Daşların, əşyaların, uçan aparatların düşməsi və digər bədbəxt hadisələr</p>
+                <div class="ferdi__auto__desc_container">
+                    <p>Qəza (toqquşma, qarşılaşma zədəsi, çevrilmə, yuvarlanma)</p>
+                    <p>Yanğın, öz-özünə alışma</p>
+                    <p>Şüşələrin sınması</p>
+                    <p>Təbii fəlakətlər</p>
+                    <p>Nəqliyyat vasitəsinin qaçırılması, qaçırılma vaxtı baş verən zədələr və üçüncü şəxslərin digər qeyri qanuni hərəkətləri</p>
+                    <p>Daşların, əşyaların, uçan aparatların düşməsi və digər bədbəxt hadisələr</p>
+                </div>
             </div>
 
 
@@ -64,16 +68,18 @@ include './include/header.php';
             </div>
 
             <div class="ferdi__auto__desc_text">
-                <p>Sərfəli qiymət</p>
-                <p>Müasir çağrı mərkəzi (24/7)</p>
-                <p>Sığorta hadisələrinin tənzimlənməsi üçün minimum şərtlər</p>
-                <p>Sizin günahınız olmadan baş verən hadisələrdə 0 AZN azadolma məbləği</p>
-                <p>“A-Qroup Mobile Concierge” mobil əlavədə SOS düyməsini basmaqla hadisə yerin qeolokasiyası və qəza komissarının dərhal hadisə yerinə çağırılması</p>
-                <p>DYP-də sənədləşmə</p>
-                <p>Sığorta hadisəsi zamanı MediClub-dan Təcili Tibbi yardım xidməti</p>
-                <p>Pulsuz evakuasiya xidməti</p>
-                <p>Hissə-hissə ödəmə imkanı</p>
-                <p>Təklif olunan servis mərkəzlərində yüksək keyfiyyətin mövcudluğu</p>
+                <div class="ferdi__auto__desc_container">
+                    <p>Sərfəli qiymət</p>
+                    <p>Müasir çağrı mərkəzi (24/7)</p>
+                    <p>Sığorta hadisələrinin tənzimlənməsi üçün minimum şərtlər</p>
+                    <p>Sizin günahınız olmadan baş verən hadisələrdə 0 AZN azadolma məbləği</p>
+                    <p>“A-Qroup Mobile Concierge” mobil əlavədə SOS düyməsini basmaqla hadisə yerin qeolokasiyası və qəza komissarının dərhal hadisə yerinə çağırılması</p>
+                    <p>DYP-də sənədləşmə</p>
+                    <p>Sığorta hadisəsi zamanı MediClub-dan Təcili Tibbi yardım xidməti</p>
+                    <p>Pulsuz evakuasiya xidməti</p>
+                    <p>Hissə-hissə ödəmə imkanı</p>
+                    <p>Təklif olunan servis mərkəzlərində yüksək keyfiyyətin mövcudluğu</p>
+                </div>
             </div>
 
             <div class="ferdi__auto__images">
