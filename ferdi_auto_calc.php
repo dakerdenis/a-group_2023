b9947b17d796d72bba6e86b3bdbf9a60b854e635
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-28 11:42:10 +0400

    added

--- ferdi_auto_calc.php
+++ ferdi_auto_calc.php
@@ -17,7 +17,7 @@ include './include/header.php';
     <?php
     include './include/navigation.php';
     ?>
-    <div class="polis__count__container">
+    <div class="polis__count__container polis__count__container_avto">
         <div class="polis__count__wrapper">
             <div class="polis__count__name ">
                 <div class="page__name_wrapper">
@@ -43,9 +43,9 @@ include './include/header.php';
                 <div class="polis__count__desc_block">
                     <p>
                         <img src="./styles/imgs/alert-circle.svg" alt="">
-                        Diqqət!
                     </p>
-                    <p>Ailə proqramı yalnız 3 və ya daha artıq şəxs üçün nəzərdə tutulub</p>
+                    <p>Qeyd edilən sığorta tarifləri dəyəri 100 000 AZN-dək olan avtomobillər üçün nəzərdə tutulub.Dəyəri 100 000 AZN-dən yüksək 
+olan avtomobillərin sığorta haqqının hesablanması üçün zəhmət olmasa *0909 nömrəsi ilə əlaqə saxlayaraq  məlumat alın.</p>
                 </div>
             </div>
 
@@ -80,44 +80,6 @@ include './include/header.php';
                                 </div>
                             </div>
                         </div>
-                        <!---------Doğum tarixi-------------->
-                        <div class="polis__count-form__input_element">
-                            <!--input desc-->
-                            <div class="polis_input_element_desc">
-                                <p>Doğum tarixi</p>
-                            </div>
-                            <!---input data--->
-                            <div class="polis_input_element_input">
-                                <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="Doğum tarixi">
-                                </div>
-                            </div>
-                        </div>                        <!---------Çəki-------------->
-                        <div class="polis__count-form__input_element">
-                            <!--input desc-->
-                            <div class="polis_input_element_desc">
-                                <p>Çəki</p>
-                            </div>
-                            <!---input data--->
-                            <div class="polis_input_element_input">
-                                <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="Çəki (kg)">
-                                </div>
-                            </div>
-                        </div>
-                        <!---------Boyu-------------->
-                        <div class="polis__count-form__input_element">
-                            <!--input desc-->
-                            <div class="polis_input_element_desc">
-                                <p>Boyu</p>
-                            </div>
-                            <!---input data--->
-                            <div class="polis_input_element_input">
-                                <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="Boyu (kg)">
-                                </div>
-                            </div>
-                        </div>
                         <!---------Əlaqə nömrəsi-------------->
                         <div class="polis__count-form__input_element">
                             <!--input desc-->
@@ -131,32 +93,6 @@ include './include/header.php';
                                 </div>
                             </div>
                         </div>
-                        <!---------İş yeri-------------->
-                        <div class="polis__count-form__input_element">
-                            <!--input desc-->
-                            <div class="polis_input_element_desc">
-                                <p>İş yeri</p>
-                            </div>
-                            <!---input data--->
-                            <div class="polis_input_element_input">
-                                <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="İş yeri">
-                                </div>
-                            </div>
-                        </div>
-                        <!---------Ünvan-------------->
-                        <div class="polis__count-form__input_element">
-                            <!--input desc-->
-                            <div class="polis_input_element_desc">
-                                <p>Ünvan</p>
-                            </div>
-                            <!---input data--->
-                            <div class="polis_input_element_input">
-                                <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="Ünvan">
-                                </div>
-                            </div>
-                        </div>
                         <!---------Email ünvanı-------------->
                         <div class="polis__count-form__input_element">
                             <!--input desc-->
@@ -166,23 +102,106 @@ include './include/header.php';
                             <!---input data--->
                             <div class="polis_input_element_input">
                                 <div class="polis_input_element_input_container">
-                                    <input type="text" placeholder="Email ünvanı">
+                                    <input type="text" placeholder="Email ünvanı ">
                                 </div>
                             </div>
                         </div>
-                    
-
-                    </div>
-
-
-                    <!---- добавление анкеты следующего члена семьи---->
-                    <div class="add_new__person_tibbi">
-                        <div class="button__add__new_person">
-                            <button id="add__new_person"><span>2</span>ci ailə üzvü</button>
+                        <!---------Avtomobilin markası və modeli-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Avtomobilin markası və modeli</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Avtomobilin markası və modeli">
+                                </div>
+                            </div>
                         </div>
+                        <!---------İstehsal ili-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>İstehsal ili</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="İstehsal ili">
+                                </div>
+                            </div>
+                        </div>
+                        <!---------Təmir servisi-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Təmir servisi</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Təmir servisi">
+                                </div>
+                            </div>
+                        </div>
+                        <!---------Franşıza-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Franşıza</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Franşıza">
+                                </div>
+                            </div>
+                        </div>
+                        <!---------Sürücü-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Sürücü</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Yalnız siğortalı">
+                                </div>
+                            </div>
+                        </div>
+                        <!---------Bonuslar-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Bonuslar</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Yeni Müştəri">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!---------Maşının bazar dəyəri-------------->
+                        <div class="polis__count-form__input_element">
+                            <!--input desc-->
+                            <div class="polis_input_element_desc">
+                                <p>Maşının bazar dəyəri</p>
+                            </div>
+                            <!---input data--->
+                            <div class="polis_input_element_input">
+                                <div class="polis_input_element_input_container">
+                                    <input type="text" placeholder="Maşının bazar dəyəri">
+                                </div>
+                            </div>
+                        </div>
+
+
+
                     </div>
-
-
                     <!---HESABLA---->
                     <div class="tibbi__calculation">
                         <div class="tibbi_calculation__block">
@@ -219,10 +238,7 @@ include './include/header.php';
 
 
         /*******************/
-
-
     </script>
-    <script src="./js/add_new_user.js"></script>
 
     <?php
     include './include/footer.php';
