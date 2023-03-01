5a7dc5a1ebb6dc2d71cf7e3abb0a53b32e5f66be
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-01 14:28:47 +0400

    added

--- korporativ_tibbi.php
+++ korporativ_tibbi.php
@@ -18,7 +18,7 @@ include './include/header.php';
     ?>
     <div class="korporativ__tibbi__container">
         <div class="korporativ__tibbi__page_name">
-            <div class="page__name_wrapper">              
+            <div class="page__name_wrapper">
                 <div class="page_name_block1 ">
                     <p>Korporatİv &nbsp;</p>
                 </div>
@@ -33,15 +33,32 @@ include './include/header.php';
             <img src="./styles/imgs/korporativ_tibbi_line.svg" alt="">
         </div>
 
-    <div class="korporativ__tibbi__desc">
-        <p>
-        Artıq 200-dən çox tanınmış beynəlxalq və yerli şirkət, səfirlik və banklar bizi seçiblər. Müasir dünyada yüksək ixtisaslı kadrların cəlb edilməsi və saxlanması üçün vacib stimullardan biri tibbi sığortadır. Çoxsaylı araşdırmalar göstərdi ki, bir şirkət seçərkən paketdə könüllü tibbi sığorta (KTS) varlığı vacib amildir.
-        </p>
-    </div>
-    
-    <div class="korporativ__tibbi__additional_name">
-        <p> <span>KTS</span>-in ÜSTÜNLÜKLƏRİ:</p>
-    </div>
+        <div class="korporativ__tibbi__desc">
+            <p>
+                Artıq 200-dən çox tanınmış beynəlxalq və yerli şirkət, səfirlik və banklar bizi seçiblər. Müasir dünyada yüksək ixtisaslı kadrların cəlb edilməsi və saxlanması üçün vacib stimullardan biri tibbi sığortadır. Çoxsaylı araşdırmalar göstərdi ki, bir şirkət seçərkən paketdə könüllü tibbi sığorta (KTS) varlığı vacib amildir.
+            </p>
+        </div>
+
+        <div class="korporativ__tibbi__additional_name">
+            <p> <span>KTS</span>-in ÜSTÜNLÜKLƏRİ:</p>
+        </div>
+
+        <div class="korporativ__tibbi__content">
+            <p>• 24 il xidmətinzdə </p>
+            <p>• MediClub и MİТRİ klinikaların xidmətlərindən eksklüziv istifadə haqqına sahibdir </p>
+            <p>• Ən yaxşı özəl klinikaların geniş şəbəkəsi </p>
+            <p>• Müasir çaqır mərkəzi (24/7) </p>
+            <p>• “A-Qroup Mobile Concierge”- mobil tətbiqi </p>
+            <p>• Xüsusi xidmət - əsas subkontraktor klinikalarında Tibbi Konsyerj </p>
+            <p>• Tibbi sığorta proqramları üzrə çalışdığı bütün həkimlərin Peşə məsuliyyəti 5 milyon ABŞ dolları məbləğində sığortalanmışdır </p>
+            <p>• Bazarda ən sadiq (loyal) müştərilərə Bəziləri 15 ildən artıq bizimlədir </p>
+            <p>• Həkimi online seçmə və online görüş təyin etmə imkanı təqdim edir </p>
+            <p>• Sizin ofisi (pulsuz) SƏTƏM üzrə və DSE istifadəçilərinin iş yerlərini qiymətləndirir və müvafiq hesabatı təqdim edirik </p>
+            <p>• Yüksək keyfiyyətli xidmət </p>
+            <p>• Müştəri yönümlülük </p>
+            <p>• Gürcüstan və Azərbaycan respublikların ərazisində tibbi yardım xidmətinin həyata keçirilməsi imkanı </p>
+            <p>• Sağlam Həyat Tərzinin təşviqi </p>
+        </div>
 
     </div>
 
