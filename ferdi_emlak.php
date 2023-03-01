c6f0049d79b988fc981e1f45858d32b4861f7529
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-01 11:46:10 +0400

    added

--- ferdi_emlak.php
+++ ferdi_emlak.php
@@ -19,20 +19,60 @@ include './include/header.php';
     <div class="ferdi_emlak_container">
         <div class="ferdi_emlak__page_name">
             <div class="page__name_wrapper">
-                <div class="page_name_block1 page_name_block_tibbi1">
+                <div class="page_name_block1 page_name_block_emlak1">
                     <p>FƏRDİ &nbsp;</p>
                 </div>
                 <div class="page_name_block2 page_name_block_tibbi">
-                    <p>Tibbi &nbsp;</p>
+                    <p>Əmlak &nbsp;</p>
                 </div>
                 <div class="page_name_block3 ">
                     <p>Sığorta</p>
                 </div>
 
             </div>
-            <img src="./styles/imgs/complaints_red.svg" alt="">
+            <img src="./styles/imgs/ferdi_emlak_red.svg" alt="">
         </div>
 
+        <div class="ferdi__emlak__name-desc">
+            <p>
+                Əmlakınızı sığortalamağın <span>ƏSL</span> vaxtıdır!
+            </p>
+        </div>
+        <div class="ferdi__emlak__name-text">
+            <p>
+                Könüllü əmlakın sığortası - əmlakınızı qorumağın və böyük maliyyə itkilərinin qarşısını almağın ən yaxşı yoludur.
+            </p>
+        </div>
+
+
+        <!----------------->
+        <div class="ferdi__emlak__content-container">
+            <!------2 block with content------>
+            <div class="ferdi__emlak__content_2block">
+                <!------------------->
+                <div class="ferdi__emlak__content__element">
+                    <div class="ferdi__emlak__element_name">
+                        <p>
+                            <span>Nəyİ</span> sığortalamaq olar?
+                        </p>
+                    </div>
+                    <div class="ferdi__emlak__element__line"></div>
+
+                    <div class="ferdi__emlak__element__text">
+
+                        <p>• Tikililər (bağ evləri, kotteclər və s.)</p>
+                        <p>• Mənzillər (daxili işləri nəzərə alaraq və almayaraq)</p>
+                        <p>• Daxili işləmə və otaq avadanlığı</p>
+                        <p>• Daşınan əmlakı (mebel; audio-, video-, məişət texnikası və s.)</p>
+                        <p>• Binadan istifadə zamanı üçüncü şəxslər qarşısında mülki məsuliyyət</p>
+                        <p>• Sənaye və kommersiya müəssisələri</p>
+                        
+                    </div>
+
+                    <img src="./styles/imgs/ferdi_emlak.svg" alt="">
+                </div>
+            </div>
+        </div>
 
     </div>
 
