01c61aefa7190c5e95fedfa6ea084c87dadf8501
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-01 13:20:58 +0400

    added

--- ferdi_seyahet.php
+++ ferdi_seyahet.php
@@ -34,6 +34,7 @@ include './include/header.php';
         </div>
         <img class="seyahet_palent" src="./styles/imgs/seyahet__plane.svg" alt="">
         <div class="ferdi__seyahet__content__container">
+            <!--------->
             <div class="ferdi__seyahet__content__element">
                 <div class="ferdi__seyahet__content__element__text">
                     <p>Dincəlməyə və ya işçi səfərə gedən hər bir kəsə xəstələnmə
@@ -43,8 +44,8 @@ include './include/header.php';
                         göstərilmiş telefon nömrələri vasitəsilə hər bir Sığortalı Assistance
                         xidməti ilə əlaqəyə girə bilər.</p>
                 </div>
-                
             </div>
+            <!--------->
             <div class="ferdi__seyahet__content__element">
                 <div class="ferdi__seyahet__content__element__text">
                     <p> Yerləşdiyiniz məkanın nə qədər uzaq məsafədə olmasından asılı olmayaraq, A-QROUP təcili yardım təşkil olunmasına zəmanət verir. Yalnız bir telefon nömrəsi Sizə, ilk baxışda çarəsiz görünən çətin vəziyyətdən çıxmağa kömək edər! Çətin vəziyyətdə mütləq yardım göstərəcək digər xidmətlər:</p>
@@ -59,8 +60,8 @@ include './include/header.php';
                     <p>• Zərərçəkənin ailəsinin məlumatlandırılması</p>
                 </div>
                 <img src="./styles/imgs/ferdi_seyahet_rect.svg" alt="">
-
             </div>
+            <!--------->
         </div>
 
         <div class="ferdi__seyahet__content__desc">
@@ -70,7 +71,7 @@ include './include/header.php';
             <div class="ferdi__seyahet__content__desc-line"></div>
             <div class="ferdi__seyahet__content__desc_text">
                 <p>
-                A-Qroup Sığorta şirkəti “Frequent Traveler” - bonus hesablama proqramına start verdi. A-Qroup-un müştərisi olmaq yalnız rahat deyil həm də sərfəli oldu. Artıq A-Qroup-dan səyahət sığortası sifariş edən Sığortalının bonus hesabına sığorta məbləğindən asılı olaraq ballar əlavə olunacaq. Misal üçün, 30 000 USD məbləğində səyahət sığortası əldə edən Sığortalının bonus hesabına hər səyahət gününə 6 bal yazılacaq. Ballar Sığortalıya bonus hesablama proqramının şərtlərinə əsasən onlardan gələcəkdə istifadə etmək imkanı verir. Kifayət qədər bal yığan Sığortalı onları yeni sığorta polisi əldə edərkən istifadə edə bilər. Misal üçün, A-Qroup-dan 7 günlük səyahət sığortası bonus olaraq almaq üçün cəmi 300 bal toplamaq kifayətdir. A-Qroup-un bonus hesablama proqramı şirkətə növbəti müraciətlərdə birbaşa faydadır. Sığortalı yığılmış balları haqqında məlumatı sığorta polisi əldə edərkən eləcə də istənilən vaxt sığorta şirkəti ilə əlaqə saxlayaraq öyrənə bilər.
+                    A-Qroup Sığorta şirkəti “Frequent Traveler” - bonus hesablama proqramına start verdi. A-Qroup-un müştərisi olmaq yalnız rahat deyil həm də sərfəli oldu. Artıq A-Qroup-dan səyahət sığortası sifariş edən Sığortalının bonus hesabına sığorta məbləğindən asılı olaraq ballar əlavə olunacaq. Misal üçün, 30 000 USD məbləğində səyahət sığortası əldə edən Sığortalının bonus hesabına hər səyahət gününə 6 bal yazılacaq. Ballar Sığortalıya bonus hesablama proqramının şərtlərinə əsasən onlardan gələcəkdə istifadə etmək imkanı verir. Kifayət qədər bal yığan Sığortalı onları yeni sığorta polisi əldə edərkən istifadə edə bilər. Misal üçün, A-Qroup-dan 7 günlük səyahət sığortası bonus olaraq almaq üçün cəmi 300 bal toplamaq kifayətdir. A-Qroup-un bonus hesablama proqramı şirkətə növbəti müraciətlərdə birbaşa faydadır. Sığortalı yığılmış balları haqqında məlumatı sığorta polisi əldə edərkən eləcə də istənilən vaxt sığorta şirkəti ilə əlaqə saxlayaraq öyrənə bilər.
                 </p>
             </div>
             <img src="./styles/imgs/ferdi_world.svg" alt="">
@@ -82,7 +83,7 @@ include './include/header.php';
 
         <div class="ferdi__seyahet__content__document">
             <p>
-            Xarici səfər sığortası üzrə tam qaydaları vasitəsi ilə yükləyə bilərsiniz.
+                Xarici səfər sığortası üzrə tam qaydaları vasitəsi ilə yükləyə bilərsiniz.
             </p>
             <a href="#">
                 <img src="./styles/imgs/ferdi_seyahet_doc.svg" alt="">
