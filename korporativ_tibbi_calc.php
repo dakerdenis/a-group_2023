4873117b54682df7f3bd325650e43d929c1e0111
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-06 10:51:22 +0400

    added

---
+++ korporativ_tibbi_calc.php
@@ -0,0 +1,43 @@
+<?php
+$title_name = "Korporativ Tibbi";
+include './include/header.php';
+
+?>
+
+<style>
+    <?php
+    include './styles/korporativ_tibbi.css';
+    ?>
+</style>
+
+
+
+<div class="main__wrapper">
+    <?php
+    include './include/navigation.php';
+    ?>
+    <div class="korporativ__tibbi__calc">
+         <br><br><br><br><br><br><br><br><br><br>
+
+        <div class="korporativ__tibbi__page_name">
+            <div class="page__name_wrapper">
+                <div class="page_name_block1 ">
+                    <p>Korporatİv &nbsp;</p>
+                </div>
+                <div class="page_name_block2 ">
+                    <p>Tibbi &nbsp;</p>
+                </div>
+                <div class="page_name_block3 ">
+                    <p>Sığortanı &nbsp;</p>
+                </div>
+                <div class="page_name_block4 ">
+                    <p>hesabla</p>
+                </div>
+            </div>
+            <img src="./styles/imgs/korporate_tibbi_calc.svg" alt="">
+        </div>
+    </div>
+
+    <?php
+    include './include/footer.php';
+    ?>
