056adbc2039b9352af4248c3d4703969f090748c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-07 10:56:56 +0400

    added

--- include/login.php
+++ include/login.php
@@ -3,10 +3,46 @@ $title_name = "Daxil olun";
 include './header.php';
 
 ?>
+<style>
+    <?php
+    include '../styles/style.css';
+    include '../styles/login.css';
+    ?>
+</style>
+
+
 
 <div class="main__wrapper">
 
+    <div class="login__page__container">
+        <!---->
+        <div class="login__page__login_form">
+            <div class="login__page__login__wrapper">
+                <form action="#" class="login__page__form">
+                    <div class="login__form__name">
+                        <p>Daxil ol</p>
+                    </div>
 
+                    <div class="login__form__name__desc">
+                        <p>
+                            Məlumatlarınızı daxil edin
+                        </p>
+                    </div>
+
+                    <div class="login__form__desc">
+                        <p>
+                            Sığorta FİN və Mobil nömrəsi dazil etməklə
+                            davam edin.
+                        </p>
+                    </div>
+
+                    <div class="login__form__input__wrapper">
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 </div>
