fcb61ea2b1efbb4750a5f8650de5285a6b58440a
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-16 09:19:01 +0400

    added

--- clinics.php
+++ clinics.php
@@ -20,6 +20,15 @@ include './include/header.php';
 
 
 
+
+            <div class="tab">
+                    <button class="tablinks" onclick="openCity(event, 'aptek')">Apteklərin siyahısı</button>
+                    <button class="tablinks" onclick="openCity(event, 'klinika')" id="defaultOpen">Klinikaların siyahısı</button>
+                    <button class="tablinks" onclick="openCity(event, 'optika')">Optiklərin siyahısı</button>
+                </div>
+
+
+
             <img class="clinics__svg" src="./styles/imgs/maliye.svg" alt="">
             <img class="clinics_red" src="./styles/imgs/maliye_red.svg" alt="">
             <img class="clinics_circle" src="./styles/imgs/maliye_circle.svg" alt="">
