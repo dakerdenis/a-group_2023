2fadead02e8049c31ae031da1e5e768e978fff47
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-03-16 09:21:11 +0400

    addded

--- clinics.php
+++ clinics.php
@@ -22,10 +22,18 @@ include './include/header.php';
 
 
             <div class="tab">
-                    <button class="tablinks" onclick="openCity(event, 'aptek')">Apteklərin siyahısı</button>
-                    <button class="tablinks" onclick="openCity(event, 'klinika')" id="defaultOpen">Klinikaların siyahısı</button>
-                    <button class="tablinks" onclick="openCity(event, 'optika')">Optiklərin siyahısı</button>
+                <button class="tablinks" onclick="openCity(event, 'aptek')">Apteklərin siyahısı</button>
+                <button class="tablinks" onclick="openCity(event, 'klinika')" id="defaultOpen">Klinikaların siyahısı</button>
+                <button class="tablinks" onclick="openCity(event, 'optika')">Optiklərin siyahısı</button>
+            </div>
+
+
+            <div class="clinics__page__content">
+                <!----SEARH FORM------>
+                <div class="clinics__search__block">
+
                 </div>
+            </div>
 
 
 
@@ -34,6 +42,25 @@ include './include/header.php';
             <img class="clinics_circle" src="./styles/imgs/maliye_circle.svg" alt="">
         </div>
     </div>
+    
+    <script>
+        function openCity(evt, cityName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+    </script>
 
 
     <?php
